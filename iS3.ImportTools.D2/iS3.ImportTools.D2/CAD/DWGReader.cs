2859cbc67fc4f5b93e3be13aa0b4bac44e2263c9
TS-Tech-CC <47548215+TS-Tech-CC@users.noreply.github.com>
2019-03-20 15:00:56 +0800

    init
    CAD数据提取demo

---
+++ iS3.ImportTools.D2/iS3.ImportTools.D2/CAD/DWGReader.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teigha.DatabaseServices;
+using Teigha.Runtime;
+
+namespace iS3.ImportTools.D2.CAD
+{
+    public class DWGReader
+    {
+        public static List<string> ReadPropertyFromFile(string inputfilename,string layerName)
+        {
+            List<string> layerList = new List<string>();    //图层列表
+            Database pDb;
+            Transaction trans;
+            List<string> result = new List<string>();
+            using (Services ser = new Services())
+            {
+                using (pDb = new Database(false, false))//不加参数会出错                 
+                {
+                    pDb.ReadDwgFile(inputfilename, FileOpenMode.OpenForReadAndReadShare, false, "");
+                    using (trans = pDb.TransactionManager.StartTransaction())
+                    {
+                        BlockTableRecord btab = (BlockTableRecord)pDb.CurrentSpaceId.GetObject(OpenMode.ForRead);
+                        List<string> bb = new List<string>();
+
+                        foreach (ObjectId btr in btab)   //所有对象遍历
+                        {
+                            DBObject obj;
+                            try
+                            {
+                                obj = trans.GetObject(btr, OpenMode.ForWrite);
+                            }
+                            catch
+                            {
+                                return null;
+                            };
+
+                            string a = obj.GetType().Name;
+                            if (!bb.Contains(a))
+                            {
+                                bb.Add(a);
+                            }
+                            #region  switch
+                            switch (obj.GetType().Name)
+                            {
+                                case "DBPoint":
+                                    DBPoint dbpoint = (DBPoint)obj;
+                                    if (dbpoint.Layer != layerName) break;
+                                   
+                                    break;
+                                case "Line":
+                                    Line line = (Line)obj;
+                                    if (line.Layer != layerName) break;
+                                    //result.Add(line);
+                                    break;
+                                case "Ellipse":
+                                    Ellipse ellipse = (Ellipse)obj;
+                                    if (ellipse.Layer != layerName) break;
+                                    //result.Add(ellipse);
+                                    break;
+                                #region
+                                case "Arc":
+                                    Arc arc = (Arc)obj;
+                                    if (arc.Layer != layerName) break;
+                                   // result.Add(arc);
+                                    break;
+                                #endregion
+                                case "Circle":
+                                    Circle circle = (Circle)obj;
+                                    if (circle.Layer != layerName) break;
+                                    result.Add(string.Format("Circle---centerX:{0},centerY:{1},radius:{2}", circle.Center.X, circle.Center.Y, circle.Radius));
+                                    //result.Add(circle);
+                                    break;
+                                case "Polyline":
+                                    Teigha.DatabaseServices.Polyline polyline = (Teigha.DatabaseServices.Polyline)obj;
+                                    if (polyline.Layer != layerName) break;
+                                    //result.Add(polyline);
+                                    break;
+                                case "DBText":
+                                    DBText dBText = (DBText)obj;
+                                    if (dBText.Layer != layerName) break;
+                                    result.Add(string.Format("DBText---TextString:{0}", dBText.TextString));
+                                    // result.Add(dBText);
+                                    break;
+                                case "Region":
+                                    Region region = (Region)obj;
+                                    if (region.Layer != layerName) break;
+                                    //result.Add(region);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            #endregion
+
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        
+        public static List<string> ReadLayerFromFile(string inputfilename)
+        {
+            List<string> layerList = new List<string>();    //图层列表
+            Database pDb;
+            Transaction trans;
+            using (Services ser = new Services())
+            {
+                using (pDb = new Database(false, false))//不加参数会出错                 
+                {
+                    pDb.ReadDwgFile(inputfilename, FileOpenMode.OpenForReadAndReadShare, false, "");
+                    using (trans = pDb.TransactionManager.StartTransaction())
+                    {
+                        #region   获取CAD内的所有图层
+
+                        LayerTable lt = (LayerTable)trans.GetObject(pDb.LayerTableId, OpenMode.ForRead);
+                        foreach (ObjectId layerId in lt)
+                        {
+                            LayerTableRecord ltr = (LayerTableRecord)trans.GetObject(layerId, OpenMode.ForRead);
+                            //ltr.Name;  图层名
+                            layerList.Add(ltr.Name);
+                        }
+                        #endregion
+                    }
+                }
+            }
+            return layerList;
+        }
+    }
+}
---
+++ iS3.ImportTools.D2/iS3.ImportTools.D2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using iS3.ImportTools.D2.CAD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Teigha.DatabaseServices;
+
+namespace iS3.ImportTools.D2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string selectFileName;
+        //选择要打开的CAD文件
+        private void InputFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".dwg";
+            dlg.Filter = "dwg documents (.dwg)|*.dwg";
+
+            // Get the selected file name and display in a TextBox 
+            if (dlg.ShowDialog() == true)
+            {
+                InputFileNameTB.Text = dlg.FileName;
+                selectFileName = dlg.FileName;
+                List<string> layerList = DWGReader.ReadLayerFromFile(dlg.FileName);
+                layerLB.ItemsSource = layerList;
+               // new DWGReader().ReadFile(dlg.FileName, @"E://shp/11.shp");
+            }
+        }
+
+        //点击查看图层内属性
+        private void layerLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectLayerName = layerLB.SelectedItem as string;
+            List<string> result= DWGReader.ReadPropertyFromFile(selectFileName, selectLayerName);
+            PropertyDG.ItemsSource = result;
+        }
+    }
+}
